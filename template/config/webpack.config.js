04e871aa67db73c6faa99b19bc1604d6ad68f820
poplark <songkang666@126.com>
2020-09-30 15:34:01 +0800

    feat: 处理 npm scripts feat: 处理 pkg 的 author

---
+++ template/config/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const webpack = require('webpack');
+const package = require('../package.json');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, '../lib'),
+    library: package.name,
+    filename: 'index.js',
+    libraryTarget: 'umd',
+  },
+  resolve: {
+    extensions: [".ts", ".js"]
+  },
+  module: {
+    rules: [{
+      test: /\.ts$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'ts-loader'
+      }
+    }]
+  },
+  plugins: [
+    new webpack.BannerPlugin(`${package.description}\n \n@author ${package.author.name}\n@license ${package.license}`),
+    new webpack.DefinePlugin({
+      __VERSION__: JSON.stringify(package.version)
+    })
+  ]
+}
