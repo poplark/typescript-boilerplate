f6eb6a79d7bbf4658b48a1f890fb4daa961df2fa
poplark <songkang666@126.com>
2020-11-30 17:57:01 +0800

    feat: use tp-share-utils in tp-webpack-boilerplate

---
+++ packages/tp-webpack-boilerplate/lib/index.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const { getReadStreamMap } = require('@popark/tp-share-utils');
+
+module.exports.run = async function() {
+  let result = new Map();
+  const dir = path.join(__dirname, '../templates');
+  const templates = [
+    'config',
+    'src',
+  ];
+  for (const item of templates) {
+    const map = await getReadStreamMap(dir, item);
+    result = new Map([...result, ...map]);
+  }
+  return result;
+}
